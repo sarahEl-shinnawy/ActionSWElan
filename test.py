5e4d7b25c1401a09feb675d8881c1733bcc8f51c
Sarah El-Shinnawy <s.Ahmed2397@nu.edu.eg>
2025-11-04 10:03:03 +0200

    finished test.py

---
+++ test.py
@@ -0,0 +1,40 @@
+import unittest
+from calculator import Calculator
+class TestCalculator(unittest.TestCase):
+    def setUp(self):
+        self.calc = Calculator()
+
+    def testadd(self):
+        result = self.calc.add(100, 7)
+        self.assertEqual(result, 107)
+        
+        result = self.calc.add(-1, 1)
+        self.assertEqual(result, 0)
+        
+        result = self.calc.add(-1, -1)
+        self.assertEqual(result, -2)
+
+    def testsubtract(self):
+        result = self.calc.subtract(100, 5)
+        self.assertEqual(result, 95)
+        
+        result = self.calc.subtract(-1, -1)
+        self.assertEqual(result, 0)
+
+    def testmultiply(self):
+        result = self.calc.multiply(7, 7)
+        self.assertEqual(result, 49)
+        
+        result = self.calc.multiply(-1, 1)
+        self.assertEqual(result, -1)
+
+    def testdivide(self):
+        result = self.calc.divide(100, 2)
+        self.assertEqual(result, 50)
+        
+        result = self.calc.divide(-60, 30)
+        self.assertEqual(result, -20)
+
+        with self.assertRaises(ValueError):
+            self.calc.divide(10, 0)
+            self.calc.divide(0, 0)
